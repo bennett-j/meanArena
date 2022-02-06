8813a324c57712097ef3593d485e07ca787b7f75
bennett-j <53816688+bennett-j@users.noreply.github.com>
2022-02-06 18:07:43 +0000

    Rewards defined

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ dynamic = True
 #
 # If partialVisibility is True, Tallon will only see part of the
 # environment.
-partialVisibility = True
+partialVisibility = False
 #
 # The limits of visibility when visibility is partial
 visibilityLimit = 6
--- tallon.py
+++ tallon.py
@@ -10,7 +10,25 @@
 import world
 import random
 import utils
-from utils import Directions
+from utils import Directions, Pose
+import config
+
+import numpy as np
+
+"""
+def createPose(self, x, y):
+    self.x = x
+    self.y = y
+
+Pose.__init__ = createPose
+"""
+
+def createPose(x,y):
+    p = Pose() 
+    p.x = x
+    p.y = y
+    return p
+
 
 class Tallon():
 
@@ -22,6 +40,48 @@ class Tallon():
 
         # What moves are possible.
         self.moves = [Directions.NORTH, Directions.SOUTH, Directions.EAST, Directions.WEST]
+
+        #############
+        # MDP stuff #
+        #############
+
+        self.states = set()
+        self.reward = {}
+
+        # define rewards
+        rEmpty = 1/config.scoreInterval
+        rBonus = config.bonusValue
+        rPit = -5
+        rMeanie = -10
+
+        bLoc = self.gameWorld.getBonusLocation()
+        pLoc = self.gameWorld.getPitsLocation()
+        mLoc = self.gameWorld.getMeanieLocation()
+
+        for x in range(config.worldLength):
+            for y in range(config.worldBreadth):
+                state = createPose(x,y)
+                
+                if utils.containedIn(state, bLoc):
+                    reward = rBonus
+                elif utils.containedIn(state, pLoc):
+                    reward = rPit
+                elif utils.containedIn(state, mLoc):
+                    reward = rMeanie
+                else:
+                    reward = rEmpty
+
+                self.states.add(state)
+                self.reward[state] = reward
+
+        
+        # show reward grid
+        grid = np.empty((10,10))
+        for state in self.states:
+            grid[state.y, state.x] = self.reward[state]
+
+        print(grid)
+
         
     def makeMove(self):
         # This is the function you need to define
@@ -49,3 +109,10 @@ class Tallon():
                 return Directions.SOUTH
 
         # if there are no more bonuses, Tallon doesn't move
+
+
+# for testing
+if __name__ == "__main__":
+    from world import World
+    gameWorld = World()
+    player = Tallon(gameWorld)
